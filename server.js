2822a770baa690a19e27d1ce45f362c033938cdb
Tominator10 <Tominator10@users.noreply.github.com>
2023-02-23 15:22:43 -0600

    Initalized express and added basic favicon

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+
+const PORT = process.env.PORT || 5163
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', function (req, res) {
+    res.render('pages/index')
+  })
+  .get('/health', function (req, res) {
+    res.status(200).send('<p>healthy</p>')
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
